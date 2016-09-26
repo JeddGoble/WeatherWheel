8ff200969379eb4a1009d0bc62aca1172979eeae
Jedd Goble <jedidiah.goble@teamaol.com>
2016-09-26 04:35:59 -0700

    Initial Commit

---
+++ WeatherWheelFixed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherWheelFixed
+//
+//  Created by jgoble52 on 9/26/16.
+//  Copyright © 2016 tangital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
